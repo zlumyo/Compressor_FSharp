f52aaf1d9e543225ab3d256919dd6460b443e18f
Vladimir Ivanov <zlumyo@gmail.com>
2013-03-17 16:21:45 +0400

    Compression with RLE algorithm was realized.
    Method compresses array of bytes to another with RLE algorithm.

--- Compressor.fs
+++ Compressor.fs
@@ -3,4 +3,47 @@
 /// Contains methods to compress a data with a various algorithms.
 type Compressor = 
     /// Performs a compressing with RLE algorithm.
-    static member CompressRLE (data : byte[]) = ()
+    static member CompressRLE (data : byte[]) =
+        let result = new ResizeArray<byte>()
+
+        // Recursively processes a data for forming the result.
+        let rec processing i current count =
+            if i = 0 then
+                processing 1 data.[i] 1y
+            elif i < data.Length then
+                if data.[i] = current then
+                    if count = 127y then
+                        result.Add(byte count)
+                        result.Add(current)
+                        processing (i+1) current 1y
+                    elif count < 0y then
+                        processing (i+1) current 2y
+                    else
+                        processing (i+1) current (count+1y)
+                else
+                    if count > 1y then
+                        result.Add(byte count)
+                        result.Add(current)
+                        processing (i+1) data.[i] 1y
+                    elif count = 1y then
+                        result.Add(byte count)
+                        result.Add(current)
+                        processing (i+1) data.[i] -2y
+                    elif count = -128y then
+                        result.[result.Count-128] <- byte count
+                        result.Add(current)
+                        processing (i+1) data.[i] 1y
+                    else
+                        result.[result.Count+(int count)] <- byte count
+                        result.Add(current)
+                        processing (i+1) data.[i] (count-1y)
+            else
+                if count > 0y then
+                    result.Add(byte count)
+                else
+                    result.[result.Count+(int count)] <- byte count
+
+                result.Add(current)
+      
+        processing 0 0uy 0y // Start processing from the first byte of data.
+        result.ToArray()
